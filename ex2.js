a077eb02d7e151c9edb3cdf151a5e1fcaef07af0
delancey <dsdelancey@gmail.com>
2020-04-18 20:04:46 -0400

    updated

--- ex2.js
+++ ex2.js
@@ -1,3 +1,27 @@
 
-//Write a function, testNum, that takes a number as an argument and returns a Promise that tests if the value is less than or greater than 5.
+//Write a function, testNum, that takes a number as an argument and returns a Promise that tests if the value is less than or greater than 50.
 
+
+
+function testNum (num) {
+    nbr = new Promise( (resolve, reject) => {
+        if ((num > 50) || (num < 50)) {
+            resolve("This number is not 50");
+        } else {
+           reject( Error("this is  50") );
+        }
+    })
+    return nbr;
+}
+
+testNum(49)
+.then( val => console.log(val) )
+.catch( err => console.log(err) )
+
+testNum(50)
+.then( val => console.log(val) )
+.catch( err => console.log(err) )
+
+testNum(51)
+.then( val => console.log(val) )
+.catch( err => console.log(err) )
